0196adac0c9dba14ecc1fc42e674c6a8af9dbba2
Lucas <lucasfm801@gmail.com>
2024-04-17 19:04:39 -0300

    ApiItens

--- src/main/java/com/example/proBarber/Controller.java
+++ src/main/java/com/example/proBarber/Controller.java
@@ -5,24 +5,68 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/")
 public class Controller {
     List<Produtos> prod = new ArrayList<>();
-    @GetMapping
-    public String raiz() {
-        return "Raiz da aplicação";
-    }
+    List<ItensDeVenda> item = new ArrayList<>();
+    String PostMensagem= "SUCESSO";
 
+    //Get e Post do Produto
+
+    @GetMapping("/")
+    String raiz(){
+        return "a";
+    }
     @GetMapping("/Prod")
     public List<Produtos> produtos(){
         return prod;
     }
     @PostMapping("/Prod")
-    public void insert(@RequestParam int id,@RequestParam String nome,@RequestParam double preco){
-        prod.add(new Produtos(id,nome,preco));
+    public ResponseEntity<Object> insert(@RequestBody Produtos produtos){
+
+        Produtos prod =new Produtos(
+                produtos.getId(),
+                produtos.getNome(),
+                produtos.getPreco()
+
+        );
+
+        Map<String,Object> respostaProd = new HashMap<>();
+        respostaProd.put("dados",prod);
+        respostaProd.put("Retorno",PostMensagem);
+
+        return ResponseEntity.status(200).body(respostaProd);
+    }
+
+    //Get e Post do item
+    @GetMapping("/item")
+    public List<ItensDeVenda> item(){
+        return item;
+    }
+
+    @PostMapping("/item")
+    public ResponseEntity<Object> insert(@RequestBody ItensDeVenda item){
+        ItensDeVenda itensDeVenda = new ItensDeVenda(
+                item.getValorRecebido(),
+                item.getServico(),
+                item.getProduto(),
+                item.getAlimento()
+        );
+
+
+        Map<String,Object> respostaItem =new HashMap<>();
+        respostaItem.put("dados",itensDeVenda);
+        respostaItem.put("Retorno",PostMensagem);
+
+
+
+        return ResponseEntity.status(200).body(respostaItem);
+
     }
 
 
