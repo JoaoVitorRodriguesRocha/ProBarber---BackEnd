9c5c6563fc3b4797b72241d81e5734f67f2a43ef
Zaanin <mateus.195922@alunosatc.edu.br>
2024-05-20 20:17:47 -0300

    subindo

---
+++ src/main/java/com/example/proBarber/model/Estoque.java
@@ -0,0 +1,74 @@
+package proBarber.proBarber.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Estoque{
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.AUTO
+    )
+    private long id;
+
+    @Column(
+            name = "Codigo", length = 10
+    )
+    private String Codigo;
+	
+    @Column(
+            name = "Nome", length = 50
+    )
+    private String Nome;
+	
+    @Column(
+            name = "Produto", length = 10
+    )
+    private String Produto;
+	
+    @Column(
+            name = "Saldo"
+    )
+    private int Saldo;
+	
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return Codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.Codigo = codigo;
+    }
+
+    public String getProduto() {
+        return Produto;
+    }
+
+    public void setProduto(String produto) {
+        this.Produto = produto;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        this.Nome = nome;
+    }
+
+    public String getSaldo() {
+        return Saldo;
+    }
+
+    public void setEmail(String saldo) {
+        this.Saldo = saldo;
+    }
+}
---
+++ src/main/java/com/example/proBarber/model/ItensPedidoVenda.java
@@ -0,0 +1,83 @@
+package proBarber.proBarber.model;
+
+import jakarta.persistence.*;
+@Entity
+public class ItensPedidoVenda {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.AUTO
+    )
+    private long id;
+
+    @Column(
+            name = "Usuario", length = 50
+    )
+    private String Usuario;
+	
+	@Column(
+            name = "Codigo", length = 50
+    )
+    private String Codigo;
+	
+    @Column(
+            name = "Preco"
+    )
+    private int Preco;
+    @Column(
+            name = "Total"
+    )
+    private String Total;
+	
+    @Column(
+            name = "Tipo", length = 1
+    )
+    private String Tipo;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return Usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.Usuario = usuario;
+    }
+
+    public String getCodigo() {
+        return Codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.Codigo = codigo }
+		
+    public int getPreco() {
+        return Preco;
+    }
+
+    public void setPreco(int prec) {
+        this.Preco = preco;
+    }
+
+    public int getTotal() {
+        return Total;
+    }
+
+    public void setTotal(int total) {
+        this.Total = total;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.Tipo = tipo;
+    }
+}
